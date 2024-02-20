8d51d1019be89588b9233ad29c7bc07fd0f0cd2d
Gaurav Sharma <gauravsharma16072001@gmail.com>
2024-02-20 20:30:41 +0530

    fixes bug

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const {Server} = require("socket.io");
-const { clear } = require("console");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
 const app = express();
-
-const io = new Server({
-    cors:true,
-    maxHttpBufferSize: 1e8 
-})
 
 app.use(bodyParser.json());
 
-io.on("connection",onConnected);
+const server = app.listen(8001, () => {
+    console.log("Socket.IO server started at port 8001");
+});
+
+const io = new Server(server, {
+    cors: true,
+    maxHttpBufferSize: 1e8
+});
+
+io.on("connection", onConnected);
 
 const allUsers = [];
-console.log(allUsers);
 const socketIdToUserMapping = new Map();
-app.listen(8000,()=>{
-    console.log("Server started at port 8000");
-})
-function onConnected(socket){
-  console.log(socket.id);
-  socket.on("new-user-joined",data=>{
-    allUsers.push(data);
-    socketIdToUserMapping.set(socket.id,data);
-    io.emit("new-user-joined",allUsers);
-  })
-  socket.on("message",data=>{
-    socket.broadcast.emit("chat-message",data);
-    console.log(data);
-  })
-  socket.on("disconnect",()=>{
-    const currUser = socketIdToUserMapping.get(socket.id);
-    allUsers.pop(currUser);
-    console.log(allUsers);
-    io.emit("new-user-joined",allUsers);
-    
-  })
-  socket.on("groupName",data=>{
-   io.emit("groupName",data)
-  })
-  socket.on("feedback",data=>{
-    socket.broadcast.emit("feedback",data)
-  })
-}
-io.listen(8001);
 
+function onConnected(socket) {
+    console.log("New connection:", socket.id);
+
+    socket.on("new-user-joined", data => {
+      allUsers.push(data);
+      console.log(allUsers);
+        socketIdToUserMapping.set(socket.id, data);
+        io.emit("new-user-joined", allUsers);
+    });
+
+    socket.on("message", data => {
+        socket.broadcast.emit("chat-message", data);
+    });
+
+    socket.on("disconnect", () => {
+        const currUser = socketIdToUserMapping.get(socket.id);
+        const index = allUsers.indexOf(currUser);
+        if (index !== -1) {
+            allUsers.splice(index, 1);
+            socketIdToUserMapping.delete(socket.id);
+            io.emit("new-user-joined", allUsers);
+        }
+    });
+
+    socket.on("groupName", data => {
+        io.emit("groupName", data);
+    });
+
+    socket.on("feedback", data => {
+        socket.broadcast.emit("feedback", data);
+    });
+}
